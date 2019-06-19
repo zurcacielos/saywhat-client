0df95c20f7458c057e37272713649c9459183027
fabian sosa escalada <fabianse@gmail.com>
2019-06-18 21:25:09 -0400

    adding material-ui and styled-components - just to experiment - we may remove this in the future

--- src/components/ChatsListScreen/ChatsNavbar.tsx
+++ src/components/ChatsListScreen/ChatsNavbar.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Toolbar} from '@material-ui/core';
+import styled from 'styled-components';
 
-const ChatsNavbar: React.FC = () => <div>Say What?</div>;
+
+const Container = styled(Toolbar)`
+  background-color: var(--primary-bg);
+  color: var(--primary-text);
+  font-size: 20px;
+  line-height: 40px;
+` as typeof Toolbar;
+const ChatsNavbar: React.FC = () => <Container>Say What?</Container>;
 
 export default ChatsNavbar;
