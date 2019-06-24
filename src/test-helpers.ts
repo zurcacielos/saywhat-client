1eb1791537dd2db51f992f5e8a9d2a4d2a8dac3d
fabian sosa escalada <fabianse@gmail.com>
2019-06-23 22:37:37 -0400

    Send message with a GraphQL mutation

--- src/test-helpers.ts
+++ src/test-helpers.ts
@@ -1,6 +1,7 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
 import { MockLink } from 'apollo-link-mock';
+
 export const mockApolloClient = (mocks: any) => {
   return new ApolloClient({
     cache: new InMemoryCache(),
